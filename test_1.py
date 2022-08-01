380b5181532ee99edf1f65541687e4dc7ef6ae20
magadip <pramods286@gmail.com>
2022-07-31 18:36:01 -0700

    Initial commit

---
+++ test_1.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+gameWindow = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Games for learning?")
+
+clock = pygame.time.Clock()
+
+crashed = False
+
+while not crashed:
+
+    for event in pygame.event.get():
+        #print(event)
+        if event.type == pygame.QUIT:
+            crashed = True
+
+        print(event)
+
+    pygame.display.update()
+    clock.tick(60)
